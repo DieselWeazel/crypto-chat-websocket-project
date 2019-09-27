43a7375cc1ea8609eececb93531581c9126e508f
August <august.filipsson@gmail.com>
2019-09-27 13:13:42 +0200

    created GUI

--- src/main/java/se/cryptosnack/demo/site/Home.java
+++ src/main/java/se/cryptosnack/demo/site/Home.java
@@ -1,14 +1,44 @@
 package se.cryptosnack.demo.site;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import se.cryptosnack.demo.model.User;
 
 @Route(value = "")
 public class Home extends VerticalLayout {
 
+  User user;
+
+  // history
+  TextArea chatField = new TextArea();
+
+  // message input field
+  TextField messageInput = new TextField();
+
+  Button sendMessage = new Button("send");
+
   public Home() {
-    // WAT
-    add(new H1("Hem"));
+    initGUI();
   }
+
+  public void initGUI() {
+    chatField.setWidth("500px");
+    chatField.setHeight("300px");
+    chatField.setEnabled(false);
+
+    messageInput.setPlaceholder("Input message");
+
+    add(new H1("Logged in as: " + "username"));
+    add(chatField);
+    HorizontalLayout horizontalLayout = new HorizontalLayout();
+    horizontalLayout.add(messageInput, sendMessage);
+    add(horizontalLayout);
+  }
+
+
 }
