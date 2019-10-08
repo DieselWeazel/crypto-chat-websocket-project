ae38d82b047c4237331a3f372b10f10b41c6e78a
jonathanharr92 <49676040+jonathanharr92@users.noreply.github.com>
2019-10-08 17:19:04 +0200

    "Merged" client into this branch.

--- src/main/java/se/cryptosnack/demo/site/Home.java
+++ src/main/java/se/cryptosnack/demo/site/Home.java
@@ -1,14 +1,78 @@
 package se.cryptosnack.demo.site;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import se.cryptosnack.demo.model.User;
 
 @Route(value = "")
 public class Home extends VerticalLayout {
 
+
+  User user;
+
+  // chatfield containing history of messages
+  TextArea chatField;
+
+  // message input field
+  TextField messageInput;
+  TextField userName;
+
+  Button sendMessage;
+  Button login;
+
+
   public Home() {
-    // WAT
-    add(new H1("Hem"));
+    initGUI();
+  }
+
+  public void initGUI() {
+    login = new Button("Connect", event -> connectClick());
+
+    chatField = new TextArea();
+    chatField.setWidth("500px");
+    chatField.setHeight("300px");
+    chatField.setEnabled(false);
+
+    messageInput = new TextField();
+    messageInput.setPlaceholder("Input message");
+
+    userName = new TextField();
+
+    sendMessage = new Button("send", event -> sendMessage(messageInput.getValue()));
+    sendMessage.setEnabled(false);
+
+
+    HorizontalLayout loginLayout = new HorizontalLayout();
+    loginLayout.add(userName, login);
+
+    add(loginLayout);
+    add(new H1("Connected as: " + "username"));
+    add(chatField);
+    HorizontalLayout messageLayout = new HorizontalLayout();
+    messageLayout.add(messageInput, sendMessage);
+    add(messageLayout);
+  }
+
+  public void connectClick() {
+    if (!userName.isEmpty()) {
+      // connect with the username userName.getValue()
+      // if connection is made, enable messageInput
+    } else {
+      Notification.show("You must enter a username to connect");
+    }
+  }
+
+  public void sendMessage(String message) {
+    if (!message.isEmpty()) {
+      // send message
+    } else {
+      Notification.show("You must enter a message to send");
+    }
   }
 }
