c8843bf0e1e15454ef58cc410695c4e7d0dd085c
August <august.filipsson@gmail.com>
2019-10-09 15:02:37 +0200

    Experimenting with Spring STOMP
    Committing before switching branch

---
+++ src/main/java/se/cryptosnack/demo/site/StompClient.java
@@ -0,0 +1,25 @@
+package se.cryptosnack.demo.site;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+public class StompClient {
+
+  private static String url = "ws://localhost:8080";
+
+  public static void main(String[] args) {
+    WebSocketClient webSocketClient = new StandardWebSocketClient();
+    WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+    stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+    stompClient.setTaskScheduler(new ConcurrentTaskScheduler());
+
+    StompSessionHandler sessionHandler = new MyStompSessionHandler();
+    stompClient.connect(url, sessionHandler);
+
+  }
+
+}
